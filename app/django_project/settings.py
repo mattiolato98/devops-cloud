f9c705e2c13836b247207c0eff6d2682a9d88200
gabrymattioli <gabrymattioli@gmail.com>
2024-11-06 12:59:34 +0100

    Updated Django settings to use environment variables

--- app/django_project/settings.py
+++ app/django_project/settings.py
@@ -1,6 +1,7 @@
 """
 Django settings for django_project project.
 """
+import os
 
 from pathlib import Path
 
@@ -8,12 +9,12 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-change-it-before-going-in-production"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 # Application definition
 
