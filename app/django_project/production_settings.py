413b1e71bf8809b440c72b0ff0e4f7cccc40cfe9
gabrymattioli <gabrymattioli@gmail.com>
2024-11-07 16:05:40 +0100

    Updated Django production_settings.py to use environment variables

--- app/django_project/production_settings.py
+++ app/django_project/production_settings.py
@@ -4,10 +4,10 @@ from django_project.settings import *
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', 
-        'NAME': 'blog',
-        'USER': 'django',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
-        'PORT': '3306',
+        'NAME': os.environ.get('MYSQL_DATABASE'),
+        'USER': os.environ.get('MYSQL_USER'),
+        'PASSWORD': os.environ.get('MYSQL_PASSWORD'),
+        'HOST': os.environ.get('MYSQL_HOSTNAME'),
+        'PORT': os.environ.get('MYSQL_PORT'),
     }
 }
