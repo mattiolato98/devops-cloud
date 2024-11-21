c8904531f10dca9adc8d458f5a395a427d74dd79
gabrymattioli <gabrymattioli@gmail.com>
2024-11-21 22:31:44 +0100

    deploy.sh: added execution via SSH on AWS server

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #! /bin/sh
 
-export $(cat .env | xargs)
-echo $CI_REGISTRY_TOKEN | docker login $CI_REGISTRY -u $CI_REGISTRY_USER --password-stdin
-docker compose pull
-docker compose up -d
+ssh -o StrictHostKeyChecking=no ec2-user@$AWS_IP_ADDRESS << 'ENDSSH'
+  export $(cat .env | xargs)
+  echo $CI_REGISTRY_TOKEN | docker login $CI_REGISTRY -u $CI_REGISTRY_USER --password-stdin
+  docker compose pull
+  docker compose up -d
+ENDSSH
