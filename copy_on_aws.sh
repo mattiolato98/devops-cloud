15535213eb325ddd74e294f4e6d75d7b0bc55bb4
gabrymattioli <gabrymattioli@gmail.com>
2024-11-21 22:31:17 +0100

    Removed copy_on_aws.sh

--- copy_on_aws.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-
-read -p "Enter AWS EC2 public IP address: " aws_public_ip
-
-chmod 400 ~/Downloads/labsuser.pem
-scp -i ~/Downloads/labsuser.pem setup_aws.sh ec2-user@$aws_public_ip:/home/ec2-user
-scp -i ~/Downloads/labsuser.pem .env docker-compose.yml deploy.sh ec2-user@$aws_public_ip:/home/ec2-user/app
