840cdb8f4b895fade19dd51f95c9aff9a22a96f6
gabrymattioli <gabrymattioli@gmail.com>
2024-11-22 11:43:33 +0100

    deploy.sh: fixed .env path

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 ssh -o StrictHostKeyChecking=no ec2-user@$AWS_IP_ADDRESS << 'ENDSSH'
-  export $(cat .env | xargs)
+  export $(cat ~/app/.env | xargs)
   echo $CI_REGISTRY_TOKEN | docker login $CI_REGISTRY -u $CI_REGISTRY_USER --password-stdin
   docker compose pull
   docker compose up -d
