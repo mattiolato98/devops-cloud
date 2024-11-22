c3ede6c51518c11752cf6e8f521e924b585988f9
gabrymattioli <gabrymattioli@gmail.com>
2024-11-22 11:44:13 +0100

    setup_aws.sh: added execution over ssh and additional commands

--- setup_aws.sh
+++ setup_aws.sh
@@ -1,21 +1,23 @@
 #! /bin/sh
 
-# Install docker and docker compose v2
-sudo yum install docker -y
-sudo service docker start
-DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
-mkdir -p $DOCKER_CONFIG/cli-plugins
-curl -SL https://github.com/docker/compose/releases/download/v2.30.3/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
-chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
+ssh -i ~/Downloads/labsuser.pem ec2-user@$AWS_IP_ADDRESS << 'ENDSSH'
+  # Install docker and docker compose v2
+  sudo yum install docker -y
+  sudo service docker start
+  DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
+  mkdir -p $DOCKER_CONFIG/cli-plugins
+  curl -SL https://github.com/docker/compose/releases/download/v2.30.3/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
+  chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
 
-# Assign ec2-user to the group docker
-sudo usermod -a -G docker ec2-user
+  # Assign ec2-user to the group docker
+  sudo usermod -a -G docker ec2-user
 
-# Generate SSH key
-ssh-keygen -t rsa -N '' -f ~/.ssh/id_rsa
-chmod 600 ~/.ssh/authorized_keys
-chmod 600 ~/.ssh/id_rsa
-cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
-cat ~/.ssh/id_rsa
+  # Generate SSH key
+  ssh-keygen -t rsa -N '' -f ~/.ssh/id_rsa
+  chmod 600 ~/.ssh/authorized_keys
+  chmod 600 ~/.ssh/id_rsa
+  cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+  private_key=cat ~/.ssh/id_rsa
+ENDSSH
 
-
+ssh-add - <<< private_key
